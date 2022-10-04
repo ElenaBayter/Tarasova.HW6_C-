5c2985238e321f4d599ef3c84588ab4d206884cd
elena tarasova <nafanichka.ev@gmail.com>
2022-10-04 17:23:04 -0400

    Exersize1

---
+++ Exersize1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+int[] array = GetArray(10, -10, 10);
+Console.WriteLine(String.Join(" ", array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+//(SECOND OPTION)
+//int[] array = new int[] {-2, 1, -3, 6, 1, -4, 5, 2, 8, -9}; 
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Ammount of positive numbers is {count}");
